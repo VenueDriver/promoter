c1ec1618c362aecd4b61f2febf0ad988372c8aaf
Ryan Alyn Porter <rap@endymion.com>
2009-06-10 16:53:29 -0400

    Generalized events.

--- app/controllers/promoter/promoter_controller.rb
+++ app/controllers/promoter/promoter_controller.rb
@@ -8,7 +8,7 @@ private
     # REST API call to get HTML for a select tag containing an event list.
     require 'uri'
     require 'net/http'
-    url = "http://venuedriver.com/api/venues/181/events/select_tag.html?username=rokvegassite&password=3jznJb75"
+    url = "http://venuedriver.com/api/venues/#{VENUE_DRIVER_VENUE_ID}/events/select_tag.html?username=#{VENUE_DRIVER_USERNAME}&password=#{VENUE_DRIVER_PASSWORD}"
     if ENV['RAILS_ENV'].eql? 'development' or ENV['RAILS_ENV'].eql? 'test'
       url = "http://localhost:3000/api/venues/410591471/events/select_tag.html?username=ryan&password=password"
     end
