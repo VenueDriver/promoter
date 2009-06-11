601810f8cf761e3b003bb62d6524bc898d5731ee
Ryan Alyn Porter <rap@endymion.com>
2009-06-11 11:58:16 -0400

    Fixed typo from conversion from VENUE_DRIVER_VENUE_ID to VENUE_DRIVER[:venue_id].

--- app/controllers/promoter/promoter_controller.rb
+++ app/controllers/promoter/promoter_controller.rb
@@ -8,7 +8,7 @@ private
     # REST API call to get HTML for a select tag containing an event list.
     require 'uri'
     require 'net/http'
-    url = "http://venuedriver.com/api/venues/#{VENUE_DRIVER[:venue_id]_VENUE_ID}/events/select_tag.html?username=#{VENUE_DRIVER[:username]}&password=#{VENUE_DRIVER[:password]}"
+    url = "http://venuedriver.com/api/venues/#{VENUE_DRIVER[:venue_id]}/events/select_tag.html?username=#{VENUE_DRIVER[:username]}&password=#{VENUE_DRIVER[:password]}"
     if ENV['RAILS_ENV'].eql? 'development' or ENV['RAILS_ENV'].eql? 'test'
       url = "http://localhost:3000/api/venues/410591471/events/select_tag.html?username=ryan&password=password"
     end
