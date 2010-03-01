2633f78c818448b8755123dc7dd74912cbbd6594
Ryan Alyn Porter <rap@endymion.com>
2010-03-01 15:28:09 -0500

    Added Event resource.  And a README.

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+class Event < ActiveResource::Base
+
+  if ENV['RAILS_ENV'].eql? 'development' or ENV['RAILS_ENV'].eql? 'test'
+    puts "Looking for Venue Driver on port 3000."
+    self.site = "http://localhost:3000/api"
+    self.user = "ryan"
+    self.password = "password"
+  else
+    self.site = "http://venuedriver.com/api"
+    self.user = VENUE_DRIVER[:username]
+    self.password = VENUE_DRIVER[:password]
+  end
+  
+end
