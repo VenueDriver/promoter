10808c71b2c378ca4d9c33e829feff23b9e800ea
Ryan Alyn Porter <rap@endymion.com>
2010-03-03 16:56:10 -0500

    Set up the event and flyer nested resources.

--- app/models/flyer.rb
+++ app/models/flyer.rb
@@ -2,11 +2,11 @@ class Event < ActiveResource::Base
 
   if ENV['RAILS_ENV'].eql? 'development' or ENV['RAILS_ENV'].eql? 'test'
     puts "Looking for Venue Driver on port 3000."
-    self.site = "http://localhost:3000/api/venues/:venue_id"
+    self.site = "http://localhost:3000/api/venues/:venue_id/events/:event_id"
     self.user = "ryan"
     self.password = "password"
   else
-    self.site = "http://venuedriver.com/api/venues/:venue_id"
+    self.site = "http://venuedriver.com/api/venues/:venue_id/events/:event_id"
     self.user = VENUE_DRIVER[:username]
     self.password = VENUE_DRIVER[:password]
   end
